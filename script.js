a78b964d8661ca78380232425dec129b35bdc102
kae333 <g2560109@edu.cc.ocha.ac.jp>
2026-05-29 02:09:33 +0900

    Summary
    ボタン追加

--- script.js
+++ script.js
@@ -1,43 +1,24 @@
 const sections = document.querySelectorAll("section");
-
 const observer = new IntersectionObserver((entries) => {
-
     entries.forEach((entry) => {
-
         if (entry.isIntersecting) {
-
             entry.target.classList.add("show");
-
         }
-
     });
-
 });
-
-
 sections.forEach((section) => {
-
     observer.observe(section);
-
 });
 const openButton =
 document.getElementById("openMapButton");
-
 const closeButton =
 document.getElementById("closeButton");
-
 const overlay =
 document.getElementById("overlay");
-
 openButton.addEventListener("click", () => {
-
 overlay.classList.add("show");
-
 });
-
 closeButton.addEventListener("click", () => {
 overlay.classList.remove("show");
-
-
 });
 console.log("button works");
