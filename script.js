7846ad869e7b27a26717b1c987d3474aa73bc572
kae333 <g2560109@edu.cc.ocha.ac.jp>
2026-05-29 01:57:02 +0900

    second commit
    水族館一覧を追加
    現状を歴史に変更
    背景部分の変更

--- script.js
+++ script.js
@@ -14,8 +14,30 @@ const observer = new IntersectionObserver((entries) => {
 
 });
 
+
 sections.forEach((section) => {
 
     observer.observe(section);
 
 });
+const openButton =
+document.getElementById("openMapButton");
+
+const closeButton =
+document.getElementById("closeButton");
+
+const overlay =
+document.getElementById("overlay");
+
+openButton.addEventListener("click", () => {
+
+overlay.classList.add("show");
+
+});
+
+closeButton.addEventListener("click", () => {
+overlay.classList.remove("show");
+
+
+});
+console.log("button works");
